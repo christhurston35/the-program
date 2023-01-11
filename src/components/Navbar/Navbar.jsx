2f7a80b3bfa3949a9d70fdae093c6d1780ecb340
Chris Thurston <christhurston35@gmail.com>
2023-01-11 18:05:55 +0200

    forgot small change in html layout to undo before refactoring everything about navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -34,23 +34,23 @@ function Navbar() {
       <div className="navbar">
         <FontAwesomeIcon className="hamburger-icon" icon={faBars} onClick={displayMenu} />
         <NavBrand handleClick={followLink} />
-        <div className={`container main ${hidden === true ? "" : "main-clicked"}`}>
-          <NavLink item="About" handleClick={followLink} />
-          <NavDropdown
-            sectionLabel="Your Job"
-            items={["Overview", "Window Schedule", "Wall Heights"]}
-            itemClick={followLink}
-            clickedClass={subHidden1 === true ? "" : "dropdown-items-clicked"}
-            handleClick={displaySubMenu1}
-          />
-          <NavDropdown
-            sectionLabel="Get Cutting"
-            items={["Plate Lists", "Wall Lists", "Rake Walls", "Fascia"]}
-            itemClick={followLink}
-            clickedClass={subHidden2 === true ? "" : "dropdown-items-clicked"}
-            handleClick={displaySubMenu2}
-          />
-        </div>
+      </div>
+      <div className={`container main ${hidden === true ? "" : "main-clicked"}`}>
+        <NavLink item="About" handleClick={followLink} />
+        <NavDropdown
+          sectionLabel="Your Job"
+          items={["Overview", "Window Schedule", "Wall Heights"]}
+          itemClick={followLink}
+          clickedClass={subHidden1 === true ? "" : "dropdown-items-clicked"}
+          handleClick={displaySubMenu1}
+        />
+        <NavDropdown
+          sectionLabel="Get Cutting"
+          items={["Plate Lists", "Wall Lists", "Rake Walls", "Fascia"]}
+          itemClick={followLink}
+          clickedClass={subHidden2 === true ? "" : "dropdown-items-clicked"}
+          handleClick={displaySubMenu2}
+        />
       </div>
     </div>
   )
